c2564622885f2e93973e1a0980b3d5ece4870c85
solaiman ali <solaimankuet03@gmail.com>
2021-04-25 15:42:50 +0600

    async in redux

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -4,8 +4,9 @@ import initialState from "./initialState";
 export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
     case types.CREATE_COURSE:
-      debugger;
-      return [...state, { ...action.course }];            
+      return [...state, { ...action.course }];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
     default:
       return state;
   }
